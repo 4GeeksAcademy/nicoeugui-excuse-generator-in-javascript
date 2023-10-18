e7b4c9c2af88c7cf91f6fa3aadd24e14180f1513
Nicolas Eugui <74990164+NicoEugui@users.noreply.github.com>
2023-10-18 18:11:28 +0000

    [ADD| Se termina proyecto de generar excusa al recargar pagina]

--- src/app.js
+++ src/app.js
@@ -7,5 +7,26 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let excuse = document.getElementById("excuse");
+  excuse.innerHTML = generateExcuse();
 };
+
+function generateExcuse() {
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let excuseWho = who[Math.floor(Math.random() * who.length)];
+  let excuseAction = action[Math.floor(Math.random() * action.length)];
+  let excuseWhat = what[Math.floor(Math.random() * what.length)];
+  let excuseWhen = when[Math.floor(Math.random() * when.length)];
+
+  return excuseWho + " " + excuseAction + " " + excuseWhat + " " + excuseWhen;
+}
